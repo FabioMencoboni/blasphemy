a6ca2c706e7030bb092ef541f079b947c0ca6527
Fabio Mencoboni <Fabio.Mencoboni@gmail.com>
2020-06-04 14:22:43 -0400

    backprop does not work

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 // loss for softmax = -log(predict@correct class) = sum (yjlogy^ j)
 // error at output for softmax = <pred> - <actual>
 use rustml::Matrix;
-use rustml::ops::MatrixMatrixOps;
+use rustml::ops::{MatrixMatrixOps,MatrixScalarOps};
 use rustml::ops_inplace::{FunctionsInPlace,MatrixScalarOpsInPlace,MatrixMatrixOpsInPlace};
 
 /// ## LIBRARY STRUCTURE
@@ -21,11 +21,13 @@ use rustml::ops_inplace::{FunctionsInPlace,MatrixScalarOpsInPlace,MatrixMatrixOp
 struct Linear {
     bias: Matrix::<f64>,
     weights: Matrix::<f64>,
+    activation: Matrix::<f64>,
     accumulated_err: Matrix::<f64>
 }
 struct Sigmoid {
     bias: Matrix::<f64>,
     weights: Matrix::<f64>,
+    activation: Matrix::<f64>,
     accumulated_err: Matrix::<f64>
 }
 struct Softmax{
@@ -35,8 +37,8 @@ struct Dropout{
 }
 
 pub enum Layer {
-    Linear{bias: Matrix::<f64>, weights:Matrix::<f64>, accumulated_err: Matrix::<f64>},
-    Sigmoid{bias: Matrix::<f64>, weights:Matrix::<f64>, accumulated_err: Matrix::<f64>},
+    Linear{bias: Matrix::<f64>, weights:Matrix::<f64>, activation: Matrix::<f64>, accumulated_err: Matrix::<f64>},
+    Sigmoid{bias: Matrix::<f64>, weights:Matrix::<f64>, activation: Matrix::<f64>, accumulated_err: Matrix::<f64>},
     Dropout{reject:f64},
     Softmax
 }
@@ -47,6 +49,13 @@ pub struct NeuralNet {
     pub alpha: f64
 }
 
+
+fn g_prime(activation: &Matrix::<f64>) -> Matrix::<f64>{
+    // the derivative of the activation is a*(1-a)
+    let act_deriv = activation.clone();
+    act_deriv.mul_scalar(-1f64).add_scalar(1f64).imule(activation);
+    act_deriv 
+}
 impl NeuralNet {
 
     pub fn new(dim_output: usize) -> NeuralNet {
@@ -56,9 +65,12 @@ impl NeuralNet {
     }
 
     pub fn linear(&mut self, dim: usize) {
+        let mut weights = Matrix::<f64>::random::<f64>(self.dim_output, dim);
+        weights.idiv_scalar(100f64);
         let next_layer: Layer = Layer::Linear{
             bias: Matrix::<f64>::random::<f64>(1, dim),
-            weights: Matrix::<f64>::random::<f64>(self.dim_output, dim),
+            weights: weights,
+            activation: Matrix::<f64>::fill(0f64, 1, dim),
             accumulated_err:  Matrix::<f64>::fill(0f64, 1, dim)
         };
         self.layers.push(next_layer);
@@ -66,9 +78,12 @@ impl NeuralNet {
     }
 
     pub fn sigmoid(&mut self, dim: usize) {
+        let mut weights = Matrix::<f64>::random::<f64>(self.dim_output, dim);
+        weights.idiv_scalar(100f64);
         let next_layer: Layer = Layer::Sigmoid{
             bias: Matrix::<f64>::random::<f64>(1, dim),
-            weights: Matrix::<f64>::random::<f64>(self.dim_output, dim),
+            weights: weights,
+            activation: Matrix::<f64>::fill(0f64, 1, dim),
             accumulated_err:  Matrix::<f64>::fill(0f64, 1, dim)
         };
         self.layers.push(next_layer);
@@ -82,29 +97,28 @@ impl NeuralNet {
 
 
 
-    pub fn forward_prop(& self, inp: &Matrix::<f64>) -> Matrix::<f64> {
-        let mut x = inp.clone();
-        for layer in self.layers.iter(){
+    pub fn forward_prop(&mut self, input: &Matrix::<f64>) -> Matrix::<f64> {
+        let mut x = input.clone();
+        for layer in self.layers.iter_mut(){
             match layer {
-                Layer::Linear{bias, weights, accumulated_err} => {
+                Layer::Linear{bias, weights, activation, accumulated_err} => {
                     x = x.mul(&weights, false, false);
                     x.iadd(&bias);
+                    *activation = x.clone();
                 },
-                Layer::Sigmoid{bias, weights, accumulated_err} =>{
+                Layer::Sigmoid{bias, weights, activation, accumulated_err} =>{
                     x = x.mul(&weights, false, false);
                     x.iadd(&bias);
                     x.isigmoid();
+                    *activation = x.clone();
                 },
                 Layer::Softmax{} =>{
-                    let mut sm = Matrix::<f64>::fill(0f64, 1, self.dim_output);
                     let mut norm: f64 = 0f64;
-                    for (smi, xi) in sm.iter_mut().zip(x.iter()){
-                        let ex: f64 = 2.71828f64.powf(*xi);
-                        *smi = ex;
-                        norm = norm + ex;
+                    for  xi in x.iter_mut(){
+                        *xi = 2.71828f64.powf(*xi);
+                        norm = norm + *xi;
                     }
-                    sm.idiv_scalar(norm);
-                    x = sm;
+                    x.idiv_scalar(norm);
                 },
                 Layer::Dropout{reject} => {
 
@@ -115,11 +129,36 @@ impl NeuralNet {
     x
     }
 
-    pub fn backprop(&mut self, inp: &Matrix::<f64>) -> f64 {
+    pub fn backprop(&mut self, inp: &Matrix::<f64>, output: &Matrix::<f64>) -> f64 {
         // take inp as an input, perform forward prop and backprop, 
         // return the AVERAGE error at tne output layer
         let mut error: Matrix::<f64> = self.forward_prop(inp);
-        0f64
+        error.isub(output);
+        let mut sum_error = 0f64;
+        for ei in error.iter(){
+            sum_error = sum_error + ei;
+        }
+        for layer in self.layers.iter_mut().rev(){
+            match layer {
+                Layer::Softmax{} => (),
+                Layer::Sigmoid{bias, weights, activation, accumulated_err} => {
+                    println!("ERR BEFORE SHAPE {}", &error);
+                    error = weights.mul(&error, false, true); // transpose W(l)*error(l+1)
+                    let act_deriv = g_prime(&activation);
+                    println!("ERR AFTER SHAPE {}", &error);
+                    println!("act_deriv SHAPE {}", &act_deriv);
+                    error.imule(&act_deriv);
+                }
+                Layer::Linear{bias, weights, activation, accumulated_err} => {
+                    error = weights.mul(&error, false, true); // transpose W(l)*error(l+1)
+                    let act_deriv = g_prime(&activation);
+                    error.imule(&act_deriv);
+                }
+                Layer::Dropout{reject} => ()
+            }
+            
+        }
+        sum_error 
     }
 }
 
@@ -131,10 +170,11 @@ fn main() {
     nn.sigmoid(12);
     nn.sigmoid(4);
     nn.softmax();
-    let mut input = Matrix::<f64>::random::<f64>(1,10);
-    input.get(0, 1).replace(&22f64).unwrap();
+    let input = Matrix::<f64>::random::<f64>(1,10);
+    let output = Matrix::<f64>::random::<f64>(1,4);
 
     let out = nn.forward_prop(&input);
+    let err = nn.backprop(&input, &output);
     println!("OUTPUT = {}", out);
     //println!("INOUT={}", input);
     
